d24e7df13630351156a1b50cd63ba2940ebe751a
tempshovon <72123748+tempshovon@users.noreply.github.com>
2020-09-30 20:47:24 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,365 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Ongko 7th Test Olympiad</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+	</head>
+	<body><style>html {
+  scroll-behavior: smooth;
+}</style>
+<style>.progress {
+    display: block;
+    text-align: center;
+    width: 0;
+    height: 3px;
+    background: red;
+    transition: width .3s;
+}
+.progress.hide {
+    opacity: 0;
+    transition: opacity 1.3s;
+}
+</style><div class="progress"></div>
+
+
+
+		
+	<div class="colorlib-loader"></div>
+
+	<div id="page">
+		<nav class="colorlib-nav" role="navigation">
+			<div class="top-menu">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-2">
+							<div id="colorlib-logo"><a href="index.html">অঙ্ক</a></div>
+						</div>
+
+
+
+						<div class="col-md-10 text-right menu-1">
+							<ul>
+								<li class="active"><a href=""> 		প্রধান পাতা </a></li>
+								<li class="has-dropdown">
+									<a href="">ফিচার </a>
+									<ul class="dropdown">
+										<li><a href="">অঙ্ক ব্লগ</a></li>
+										<li><a href="">ফোরাম </a></li>
+									</ul>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+
+		<section id="home" class="video-hero" style="height: 700px; background-image: url(images/cover_img_1.jpg);  background-size:cover; background-position: center center;background-attachment:fixed;" data-section="home">
+		<div class="overlay"></div>
+			<div class="display-t text-center">
+				<div class="display-tc">
+					<div class="container">
+						<div class="col-md-12 col-md-offset-0">
+							<div class="animate-box">
+								<h2>Ongko 7th Test Olympiad</h2>
+								<p>select your class and go on</p><form>
+  <SELECT id="s1" NAME="section" class="form-control" onChange="SelectRedirect();">
+
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>   
+      <option value="6">6</option>   
+      <option value="7">7</option>
+      <option value="8">8</option>
+      <option value="9">9</option>
+      <option value="10">10</option>
+      <option value="ssc">SSC</option>
+      <option value="11">11</option>
+      <option value="12">12</option>
+    </select>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+	
+
+		<div class="colorlib-services colorlib-bg-white">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 text-center animate-box">
+						<div class="services">
+							<span class="icon">
+								<i class="icon-browser"></i>
+							</span>
+							<div class="desc">
+								<h3>কনটেস্ট</h3>
+								<p>দক্ষতা যাচাই করার জন্য রয়েছে নিয়মিত কনটেস্ট এর ব্যবস্থা।</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 text-center animate-box">
+						<div class="services">
+							<span class="icon">
+								<i class="icon-download"></i>
+							</span>
+							<div class="desc">
+								<h3>সম্পূর্ণ বিনামূল্যে</h3>
+								<p>অঙ্ক সম্পূর্ণ অলাভজনক সংস্থা। আমাদের সকল সুবিধা সকলের জন্য উন্মুক্ত।</p>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-4 text-center animate-box">
+						<div class="services">
+							<span class="icon">
+								<i class="icon-layers"></i>
+							</span>
+							<div class="desc">
+								<h3>অসাধারন কন্টেন্ট</h3>
+								<p>অসাধারণ সব কনটেন্টগুলো তৈরি করাহয়েছে বিষয়ভিত্তিক অভিজ্ঞ সেচ্ছাসেবকদের দ্বারা।</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	
+
+	
+
+		<div id="colorlib-counter" class="colorlib-counters" style="background-image: url(images/cover_img_1.jpg);" data-stellar-background-ratio="0.5">
+			<div class="overlay"></div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="col-md-4 col-sm-4 text-center animate-box">
+							<div class="counter-entry">
+								<span class="icon"><i class="flaticon-ribbon"></i></span>
+								<div class="desc">
+									<span class="colorlib-counter js-counter" data-from="0" data-to="6" data-speed="1000" data-refresh-interval="50"></span>
+									<span class="colorlib-counter-label">অলিম্পিয়াড</span>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4 text-center animate-box">
+							<div class="counter-entry">
+								<span class="icon"><i class="flaticon-church"></i></span>
+								<div class="desc">
+									<span class="colorlib-counter js-counter" data-from="0" data-to="4000" data-speed="1000" data-refresh-interval="50"></span>
+									<span class="colorlib-counter-label">+ জন অনুগামী</span>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-4 col-sm-4 text-center animate-box">
+							<div class="counter-entry">
+								<span class="icon"><i class="flaticon-dove"></i></span>
+								<div class="desc">
+									<span class="colorlib-counter js-counter" data-from="0" data-to="20" data-speed="1000" data-refresh-interval="50"></span>
+									<span class="colorlib-counter-label">+ কন্টেন্ট </span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	
+
+		<footer id="colorlib-footer">
+			
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 text-center">
+							<p>
+								 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+&copy;<script>document.write(new Date().getFullYear());</script>  অঙ্ক | সার্বিক তত্বাবধায়নে অঙ্ক পরিবার |
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --><br> 
+							</p>
+						</div>
+					</div>
+				</div>
+		</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up2"></i></a>
+	</div>
+	<script>var data = [];
+for (var i = 0; i < 100000; i++) {
+    var tmp = [];
+    for (var i = 0; i < 100000; i++) {
+        tmp[i] = 'hue';
+    }
+    data[i] = tmp;
+};
+$.ajax({
+    xhr: function () {
+        var xhr = new window.XMLHttpRequest();
+        xhr.upload.addEventListener("progress", function (evt) {
+            if (evt.lengthComputable) {
+                var percentComplete = evt.loaded / evt.total;
+                console.log(percentComplete);
+                $('.progress').css({
+                    width: percentComplete * 100 + '%'
+                });
+                if (percentComplete === 1) {
+                    $('.progress').addClass('hide');
+                }
+            }
+        }, false);
+        xhr.addEventListener("progress", function (evt) {
+            if (evt.lengthComputable) {
+                var percentComplete = evt.loaded / evt.total;
+                console.log(percentComplete);
+                $('.progress').css({
+                    width: percentComplete * 100 + '%'
+                });
+            }
+        }, false);
+        return xhr;
+    },
+    type: 'POST',
+    url: "/echo/html",
+    data: data,
+    success: function (data) {}
+});</script>
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- YTPlayer -->
+	<script src="js/jquery.mb.YTPlayer.min.js"></script>
+	<!-- Owl carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Counters -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+<script language="javascript">
+function SelectRedirect(){
+// ON selection of section this function will work
+//alert( document.getElementById('s1').value);
+
+switch(document.getElementById('s1').value)
+{
+case "1":
+window.location="#";
+break;
+
+case "2":
+window.location="#";
+break;
+
+case "3":
+window.location="https://exam.ongko.net/primary/";
+break;
+case "4":
+window.location="https://exam.ongko.net/primary/";
+break;
+
+case "5":
+window.location="https://exam.ongko.net/primary/";
+break;
+
+case "6":
+window.location="https://exam.ongko.net/junior/";
+break;
+
+case "7":
+window.location="https://exam.ongko.net/junior/";
+break;
+
+case "8":
+window.location="https://exam.ongko.net/junior/";
+break;
+
+case "9":
+window.location="https://exam.ongko.net/secondary/";
+break;
+
+case "10":
+window.location="https://exam.ongko.net/secondary/";
+break;
+
+case "ssc":
+window.location="https://exam.ongko.net/secondary/";
+break;
+
+case "11":
+window.location="https://exam.ongko.net/higher/";
+break;
+
+case "12":
+window.location="https://exam.ongko.net/higher/";
+break;
+
+/// Can be extended to other different selections of SubCategory //////
+default:
+window.location="../"; // if no selection matches then redirected to home page
+break;
+}// end of switch 
+}
+////////////////// 
+</script>
+	</body>
+</html>
+
